d204f2955acc59dcb4752aa92ea11d1446c70f3f
naumtinga <naumtinga@gmail.com>
2024-08-15 11:52:22 +0200

    fixed the code a bit

--- src/app/models/subProjecto/subProjecto.ts
+++ src/app/models/subProjecto/subProjecto.ts
@@ -1,6 +1,6 @@
 import { Actividade } from "../actividade/actividade.model";
 import { Beneficiario } from "../beneficiario/beneficiario.model";
-import { Projecto } from "../projecto/projecto.model";
+import { Projecto } from "../projecto/projecto";
 
 export class SubProjecto {
     id: any;
